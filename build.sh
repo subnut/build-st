bd3e0adb1b8d9de44388a7acdb8eff5766afb8ef
Subhaditya Nath <sn03.general@gmail.com>
2021-04-01 12:59:22 +0530

    Improvements, Ctrl+Alt+{C,V} copy/paste mappings

--- build.sh
+++ build.sh
@@ -6,20 +6,20 @@ test -d st && rm -rvf st
 git clone git://git.suckless.org/st --depth 1
 cd st
 
-### Patches
+### Patches ###########################################
 echo '# Applying patches'
 
-# Bold is not bright
+## Bold is not bright
 # https://st.suckless.org/patches/bold-is-not-bright
 echo '- Bold is not bright'
 curl https://st.suckless.org/patches/bold-is-not-bright/st-bold-is-not-bright-20190127-3be4cf1.diff | patch
 
-# Boxdraw
+## Boxdraw
 # https://st.suckless.org/patches/boxdraw
 echo '- Boxdraw'
 curl https://st.suckless.org/patches/boxdraw/st-boxdraw_v2-0.8.3.diff | patch
 
-# Scrollback
+## Scrollback
 # https://st.suckless.org/patches/scrollback
 echo '- Scrollback'
 curl https://st.suckless.org/patches/scrollback/st-scrollback-0.8.4.diff | patch
@@ -28,7 +28,8 @@ echo '- Mouse scrollback support'
 echo "  NOTE: First hunk (config.def.h) is bound to FAIL, it's OK"
 curl https://st.suckless.org/patches/scrollback/st-scrollback-mouse-altscreen-20200416-5703aa0.diff | patch
 
-### Patches End
+echo '# Done patching'
+### Patches End ###########################################
 
 echo '# Copying config'
 cp -v config.def.h config.h
